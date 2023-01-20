a62408b3802ab1903ed73ae84774549d870b0e7a
Albert Sanchez <albert.san.iza@gmail.com>
2023-01-19 22:02:05 -0800

    udpate challenges

---
+++ snippets/01-Calendar.js
@@ -0,0 +1,27 @@
+// Instructions
+// Run this file by typing `node 01-Calendar.js` in the terminal
+// Please write your code after each comment
+
+// 10 coding challenges for beginners
+
+// Challenge 1: Print current date and time
+const currentDate = new Date();
+console.log("🚀 ~ currentDate", currentDate);
+
+// Challenge 2: Print the current month
+
+// Challenge 3: Print the first letter of the current day
+
+// Challenge 4: Print the number of days in the current month
+
+// Challenge 5: Print the number of days left in the current month
+
+// Challenge 6: Print the number of hours left in the current day
+
+// Challenge 7: Print the number of minutes left in the current hour
+
+// Challenge 8: Print the number of seconds left in the current minute
+
+// Challenge 9: Print the number of milliseconds left in the current second
+
+// Challenge 10: Print the number of days since January 1st
